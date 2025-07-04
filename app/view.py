9d27263524d773dca05ed7566afb7325633bb2d0
andre-holler <seu@email.com>
2025-07-04 12:31:08 -0300

    v6

--- app/view.py
+++ app/view.py
@@ -1,6 +1,7 @@
 from app import app, db
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 from app.forms import ContatoForm
+from app.models import Contato
 
 @app.route('/')
 def homepage():
@@ -46,7 +47,21 @@ def contato():
 
     if form.validate_on_submit():
         form.save()
-    
+        return redirect(url_for('homepage'))
 
     return render_template('contato.html', context=context, form=form)
 
+
+@app.route('/contato/lista/')
+def contatoLista():
+
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa', '')
+    dados = Contato.query.order_by(Contato.nome)
+
+    if pesquisa != '':
+        dados = dados.filter(Contato.nome == pesquisa)
+
+    context = {'dados':dados.all()}
+
+    return render_template('contato_lista.html', context=context)
